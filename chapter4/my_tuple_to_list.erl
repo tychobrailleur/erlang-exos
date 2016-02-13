743a020aa97a4db9499bccd29e056c2c21e01f0e
Sébastien Le Callonnec <sebastien@weblogism.com>
2016-02-13 09:20:39 +0000

    Add test.

--- chapter4/my_tuple_to_list.erl
+++ chapter4/my_tuple_to_list.erl
@@ -1,8 +1,14 @@
 -module(my_tuple_to_list).
--export([my_tuple_to_list/1]).
+-export([my_tuple_to_list/1, test/0]).
 
 my_tuple_to_list(T) ->
     my_tuple_to_list(T, []).
+
+test() ->
+    [] = my_tuple_to_list({}),
+    [1] = my_tuple_to_list({1}),
+    [1,2] = my_tuple_to_list({1,2}),
+    tests_worked.
 
 my_tuple_to_list(T, Acc) when tuple_size(T) == 0 -> lists:reverse(Acc);
 my_tuple_to_list(T, Acc) when tuple_size(T) > 0 ->
