654c685ae79d7049b9fd06553805b136a4af7316
Sébastien Le Callonnec <sebastien@weblogism.com>
2016-02-13 16:01:35 +0000

    Add python's isoweekday.

--- chapter4/my_time_func.erl
+++ chapter4/my_time_func.erl
@@ -1,11 +1,9 @@
 -module(my_time_func).
--export([test/0]).
-
+-export([test/0, weekday/3, days_before_year/1]).
 
 test() ->
     my_time_func(timer, sleep, [1500]),
     my_date_string().
-
 
 %% Uses deprecated erlang:now()...
 %% my_time_func(Module, F, Args) ->
@@ -24,3 +22,38 @@ my_date_string() ->
     {Year, Month, Day} = erlang:date(),
     {Hour, Minute, Second} = erlang:time(),
     io:format("Today is ~p/~p/~p ~p:~p:~p.~n", [Day, Month, Year, Hour, Minute, Second]).
+
+
+%%% Implement some of the datetime python functions
+
+
+
+days_before_year(Year) ->
+    Y = Year - 1,
+    Y*365 + Y div 4 - Y div 100 + Y div 400.
+
+days_in_month(Year, Month) when Month == 2 ->
+    case calendar:is_leap_year(Year) of
+        true -> 29;
+        false -> 28
+    end;
+days_in_month(_, Month) when Month /= 2 ->
+    case lists:member(Month, [1, 3, 5, 7, 8, 10, 12]) of
+        true -> 31;
+        false -> 30
+    end.
+
+days_before_month(Month, Year) ->
+    days_before_month(Month, Year, 0).
+
+days_before_month(1, _, Days) ->
+    Days;
+days_before_month(Month, Year, Days) ->
+    days_before_month(Month-1, Year, days_in_month(Month, Year) + Days).
+
+weekday(Day, Month, Year) ->
+    Weekday =  (days_before_year(Year) + days_before_month(Month, Year) + Day) rem 7,
+    case Weekday of
+        0 -> 7;
+        _ -> Weekday
+    end.
