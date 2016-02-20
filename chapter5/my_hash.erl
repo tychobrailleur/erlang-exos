bd40b911101c7242ab1a0f2e9e9e92525fd791d2
Sébastien Le Callonnec <sebastien@weblogism.com>
2016-02-20 13:20:14 +0000

    Add is_empty and invert.

--- chapter5/my_hash.erl
+++ chapter5/my_hash.erl
@@ -1,12 +1,16 @@
 -module(my_hash).
--export([delete_if/2, test/0]).
+-export([delete_if/2, is_empty/1, invert/1, test/0]).
 
 test() ->
-    Hash = delete_if(fun(_K,_V) -> _V rem 2 /= 0 end, #{ "one" => 1, "two" => 2 }),
-    error = maps:find("one", Hash),
-    {ok, 2} = maps:find("two", Hash),
+    test_delete_if(),
+    test_is_empty(),
+    test_invert(),
     test_worked.
 
+test_delete_if() ->
+    Hash = delete_if(fun(_K,_V) -> _V rem 2 /= 0 end, #{ "one" => 1, "two" => 2 }),
+    error = maps:find("one", Hash),
+    test_worked.
 
 delete_if(Pred, Map) ->
     maps:fold(fun (_K, _V, Acc) ->
@@ -15,3 +19,20 @@ delete_if(Pred, Map) ->
                           _ -> Acc
                       end
               end, #{}, Map).
+
+
+test_is_empty() ->
+    {ok, 2} = maps:find("two", #{ "one" => 1, "two" => 2 }),
+    false = is_empty(#{ "one" => 1 }),
+    true = is_empty(#{}),
+    test_worked.
+
+
+is_empty(_H) when map_size(_H) == 0 -> true;
+is_empty(_H) -> false.
+
+test_invert() ->
+    { ok, 1 } = maps:find("one", invert(#{ 1 => "one" })),
+    tests_worked.
+
+invert(_H) -> maps:fold(fun (_K, _V, Acc) -> Acc#{ _V => _K }  end, #{}, _H).
