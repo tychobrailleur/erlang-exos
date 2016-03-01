166222cb6696101f3d43c2d238d682570c23f3f8
Sébastien Le Callonnec <sebastien@weblogism.com>
2016-03-01 21:36:00 +0000

    Cleanup.

--- chapter9/sliding.erl
+++ chapter9/sliding.erl
@@ -1,12 +1,12 @@
 -module(sliding).
--compile([export_all]).
-
+-export([sliding/2, test/0]).
 
 test() ->
     [[1,2],[2,3],[3,4]] = sliding([1, 2, 3, 4], 2),
     [["I", "am"], ["am", "legend"]] = sliding(["I", "am", "legend"], 2),
     tests_worked.
 
+%% Returns a list holding a sliding window of the input list, and of size Size.
 -spec sliding(list(), pos_integer()) -> list().
 sliding(List, Size) ->
     sliding(List, Size, []).
