f827c8a6b6ce610676fef17fe8860d54cf753b22
Sébastien Le Callonnec <sebastien@weblogism.com>
2016-03-14 22:58:53 +0000

    Exercise 13.4.

---
+++ chapter13/parrot.erl
@@ -0,0 +1,29 @@
+-module(parrot).
+-export([parrot/0, parrot_creator/0, start/0]).
+
+
+parrot() ->
+    receive
+    after
+        5000 -> io:format("I'm still alive.~n"),
+                parrot()
+    end.
+
+
+parrot_creator() ->
+    { Pid, Ref } = spawn_monitor(?MODULE, parrot, []),
+    register(pearl_jam, Pid),
+
+    receive
+        { 'DOWN', Ref, process, Pid, Why } ->
+            io:format("Process ~p died: ~p.~n", [Pid, Why]),
+            parrot_creator()
+    end.
+
+start() ->
+    spawn(?MODULE, parrot_creator, []).
+
+
+%% Try killing the peal_jam process with the following:
+%%
+%% ` xit(whereis(pearl_jam), kill).`
