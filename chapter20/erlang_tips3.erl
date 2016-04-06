957ba6b2906f847720a4aecdafb5c55159756b78
Sébastien Le Callonnec <sebastien@weblogism.com>
2016-04-06 22:51:03 +0100

    Exercise 20.3.

---
+++ chapter20/erlang_tips3.erl
@@ -0,0 +1,114 @@
+-module(erlang_tips3).
+-export([init/0, setup_schema/0, add_user/3, get_user/1, list_users/0, add_tip/3, get_tip/1,
+         list_tips/0, get_abuse/1, add_abuse/2, list_abuse/0, delete_tables/0, count_recent_tips/1,
+         count_different_ips/1]).
+-include_lib("stdlib/include/qlc.hrl").
+
+-define(SALT_LENGTH, 12).
+-define(SECONDS_IN_DAY, 24*3600*60).
+
+-record(user, { email, name, password, salt }).
+-record(tip, { url, description, review_date, user }).
+-record(abuse, { timestamp, ip, user }).
+
+
+init() ->
+    random:seed(erlang:time_offset(),
+                erlang:monotonic_time(),
+                erlang:unique_integer()).
+
+setup_schema() ->
+    mnesia:create_schema([node()]),
+    mnesia:start(),
+    mnesia:create_table(users, [{ram_copies, [node()]},
+                                {record_name, user},
+                                {attributes, record_info(fields, user)}]),
+    mnesia:create_table(tips, [{ram_copies, [node()]},
+                               {record_name, tip},
+                               {attributes, record_info(fields, tip)}]),
+    mnesia:create_table(abuse, [{ram_copies, [node()]},
+                                {record_name, abuse},
+                                {attributes, record_info(fields, abuse)}]),
+    mnesia:stop().
+
+random_string(Length) ->
+    lists:map(fun(_) -> crypto:rand_uniform(48, 122) end, lists:seq(1, Length)).
+
+
+add_user(Name, Email, Password) ->
+    Salt = random_string(?SALT_LENGTH),
+    Row = #user{email=Email, name=Name, password=crypto:hash(sha256, Password ++ Salt), salt=Salt},
+    F = fun() ->
+                mnesia:write(users, Row, write)
+        end,
+    mnesia:transaction(F).
+
+get_user(Email) ->
+    F = fun() -> mnesia:read(users, Email) end,
+    { atomic, Val } = mnesia:transaction(F),
+    Val.
+
+list_users() ->
+    do(qlc:q([ {U#user.email, U#user.name} || U <- mnesia:table(users) ])).
+
+add_tip(Url, Description, User) ->
+
+    RecentTips = count_recent_tips(User),
+    case RecentTips > 10 of
+        true -> { error, too_many_tips };
+        false -> DifferentIps = count_different_ips(User),
+                 case DifferentIps > 3 of
+                     true -> { error, too_many_different_ips };
+                     false -> do_add_tip(Url, Description, User)
+                 end
+    end.
+
+do_add_tip(Url, Description, User) ->
+    Row = #tip{url=Url, description=Description, user=User, review_date=erlang:localtime()},
+    F = fun() ->
+                mnesia:write(tips, Row, write)
+        end,
+    mnesia:transaction(F).
+
+count_recent_tips(User) ->
+    Tips = do(qlc:q([ T || T <- mnesia:table(tips), T#tip.user =:= User, calendar:datetime_to_gregorian_seconds(erlang:localtime()) - calendar:datetime_to_gregorian_seconds(T#tip.review_date) < ?SECONDS_IN_DAY ])),
+    length(Tips).
+
+count_different_ips(User) ->
+    Abuse = do(qlc:q([ A#abuse.ip || A <- mnesia:table(abuse), A#abuse.user =:= User, calendar:datetime_to_gregorian_seconds(erlang:localtime()) - calendar:datetime_to_gregorian_seconds(A#abuse.timestamp) < (?SECONDS_IN_DAY*3) ], {unique, true})),
+    length(Abuse).
+
+
+get_tip(Url) ->
+    F = fun() -> mnesia:read(tip, Url) end,
+    { atomic, Val } = mnesia:transaction(F),
+    Val.
+
+list_tips() ->
+    do(qlc:q([ T || T <- mnesia:table(tips) ])).
+
+get_abuse(Ip) ->
+    F = fun() -> mnesia:read(abuse, Ip) end,
+    { atomic, Val } = mnesia:transaction(F),
+    Val.
+
+
+add_abuse(Ip, User) ->
+    Row = #abuse{timestamp=erlang:localtime(), ip=Ip, user=User},
+    F = fun() ->
+                mnesia:write(Row)
+        end,
+    mnesia:transaction(F).
+
+
+list_abuse() ->
+    do(qlc:q([ A || A <- mnesia:table(abuse) ])).
+
+
+delete_tables() ->
+    lists:foreach(fun(Table) -> mnesia:delete_table(Table) end, [abuse, tips, users]).
+
+do(Q) ->
+    F = fun() -> qlc:e(Q) end,
+    { atomic, Val } = mnesia:transaction(F),
+    Val.
